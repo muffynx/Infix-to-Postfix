e8036286469ee1bd629c42b7997d9f4e853c2448
muffyn_pp <72650070+muffynx@users.noreply.github.com>
2023-01-20 17:46:36 +0700

    update

---
+++ Batch.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\muffyn\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\muffyn\Documents\GitHub\Infix-to-Postfix\Infix-to-Postfix.py"
+pause
--- Infix-to-Postfix.py
+++ Infix-to-Postfix.py
@@ -1,3 +1,6 @@
+OPERATORS = set(['+', '-', '*', '/', '(', ')', '^'])  
+PRIORITY = {'+':1, '-':1, '*':2, '/':2, '^':3} 
+
 def count(expression):
     count = 0
     for char in expression:
@@ -18,76 +21,73 @@ def infix_to_postfix(expression):
                 output += myStack.pop()
             myStack.pop()
         else: 
-            while myStack and myStack[-1]!='(' and PRIORITY[ch]<=PRIORITY[myStack[-1]]:
+            while myStack and myStack[-1] != '(' and PRIORITY[ch] <= PRIORITY[myStack[-1]]:
                 output += myStack.pop()
             myStack.append(ch)
     while myStack:      
         output += myStack.pop()
     return output 
 
-OPERATORS = set(['+', '-', '*', '/', '(', ')', '^'])  
-PRIORITY = {'+':1, '-':1, '*':2, '/':2, '^':3} 
-expression = input('Input Expression :') 
-infix_to_postfix=infix_to_postfix(expression)
-number_of_alphabets = count(expression) #return อะไรก็เอาอันนั้นเลย เช่น count(expression)
-max = number_of_alphabets
-s=[None]*max
-
-top=-1
 def isFull():
-    global top,max
-    if top==max-1:
+    global top, max
+    if top == max - 1:
         return True
     return False
+
 def doPush(a):
-    global top,s,max
-    if (not isFull()):
+    global top, s, max
+    if not isFull():
         top +=1
         s[top] = a
     else:
         print("the stack is Full")
+
 def isEmpty():
     global top
-    if top ==-1:
+    if top == -1:
         return True
     return False
+
 def dopop():
-    global top,s
+    global top, s
     if not isEmpty():
-        a=s[top]
-        top-=1
+        a = s[top]
+        top -= 1
         return a
     return -1
+
 def evalpostfix(Enter_postfix):
-    
     global top
     for ch in Enter_postfix:
-        if ch in ["+","-","*","/"]:
-            r=dopop()
-            l=dopop()
-            if ch in "+":
-                x=l+r
+        if ch in ["+", "-", "*", "/"]:
+            r = dopop()
+            l = dopop()
+            if ch == "+":
+                x = l + r
                 doPush(x)
-            elif ch in "-":
-                x=l-r
+            elif ch == "-":
+                x = l - r
                 doPush(x)
-            elif ch in "*":
-                x=l*r
+            elif ch == "*":
+                x = l * r
                 doPush(x)
-            elif ch in "/":
-                x=l/r
+            elif ch == "/":
+                x = l / r
                 doPush(x)
         else:
-            x=int(input("Enter the value : "))
+            x = int(input("Enter the value : "))
             doPush(x)
-     
     return dopop()
-Enter_postfix=(infix_to_postfix)
-res=evalpostfix(Enter_postfix)
-print("The Result is: ",res)
-
-
-
-
-
-
+def reset_variables():
+    global top, s, max
+    top = -1
+    s = [None] * max
+while True:
+    expression = input('Input Expression :')
+    postfix_expression = infix_to_postfix(expression)
+    max = count(expression)
+    s = [None] * max
+    top = -1
+    res = evalpostfix(postfix_expression)
+    print("The Result is: ", res)
+    reset_variables()
